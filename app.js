60ec2110fe6fbc023b697dd2b689ebae942e85f4
Gabrielle Poletti <89078209+gabrielleeee@users.noreply.github.com>
2023-02-16 14:03:35 +0000

    (16/02) Criação do primeiro model e migration

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+// Carrega as variáveis de ambiente do arquivo
+// .env para a aplicação
+require('dotenv').config()
+
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -8,6 +12,19 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+//Conexão colm Banco de Dados-------------------------------------------------
+
+const db = require('./models')
+
+try {
+    db.sequelize.authenticate()
+    console.log('SEQUELIZE: connection has been estabilished succesfully.')
+}
+catch(error) {
+    console.error('*SEQUELIZE: unable to connect to the database: ', error)
+    process.exit(1) //encerra o servidor com erro
+}
+//-----------------------------------------------------------------------------
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
