7a1dea17a3cec6ad33b8f6d487d04b4536a36ab0
Gabrielle Poletti <89078209+gabrielleeee@users.noreply.github.com>
2023-02-28 14:03:22 +0000

    28/02 MÉTODO RETRIEVE

--- controllers/user.js
+++ controllers/user.js
@@ -23,4 +23,29 @@ controller.create = async (req, res) => {
     }
 }
 
+controller.retrieve = async(req, res) => {
+    try{
+        const data = await User.findAll() //findAll dá um select*
+        //HTTP 200: OK (implícito)
+        res.send(data)
+    }
+    catch(error){
+        console.error(error)
+    }
+}
+
+controller.retrieveOne = async(req, res) => {
+    try{
+        const data = await User.findByPk(req.params.id) //findAll dá um select*
+        //HTTP 200: OK (implícito)
+        if(data) res.send(data)
+
+        //HTTP 404: Not Found
+        else res.status(404).end()
+        
+    }
+    catch(error){
+        console.error(error)
+    }
+}
 module.exports = controller
--- routes/users.js
+++ routes/users.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const controller = require('../controllers/user')
 
 router.post('/', controller.create)
+router.get('/', controller.retrieve)
 
 module.exports = router;
